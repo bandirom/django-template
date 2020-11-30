02b9d39bff9f1995ea20aab7714065333b7fd287
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2020-11-30 13:19:58 +0200

    defender upd

--- web/main/admin.py
+++ web/main/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from rest_framework.authtoken.admin import Token
+
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ()
+    list_display = ('email', 'first_name', 'last_name',)
+
+    fieldsets = (
+        (None, {'fields': ('password',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('password1', 'password2'),
+        }),
+    )
+
+
+title = settings.MICROSERVICE_TITLE
+
+admin.site.site_title = title
+admin.site.site_header = title
+admin.site.site_url = '/'
+admin.site.index_title = title
+
+admin.site.unregister(Group)
+admin.site.unregister(Token)
