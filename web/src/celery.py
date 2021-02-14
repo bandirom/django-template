a2fff71ce505da00d9aeb9074e049aadeb047583
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-02-14 10:52:40 +0200

    added celery

---
+++ web/src/celery.py
@@ -0,0 +1,8 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+
+app = Celery("src")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
