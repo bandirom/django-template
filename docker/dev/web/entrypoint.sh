584fcac0bc56461784cb89c271ce2798e6a2ea54
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-02-14 10:58:36 +0200

    added uvicorn support

--- docker/dev/web/entrypoint.sh
+++ docker/dev/web/entrypoint.sh
@@ -10,3 +10,7 @@ echo "PostgreSQL started"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
+
+# If you use Uvicorn please also run collectstatic command
+#python manage.py collectstatic --no-input
+#uvicorn src.asgi:application --host 0.0.0.0 --port 8000 --log-level debug --reload
