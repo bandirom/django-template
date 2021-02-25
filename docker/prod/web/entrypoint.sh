2a67c4278884d8e33fee1b6f9a21fc0c374bc34e
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-02-25 21:48:17 +0200

    fixed some issues

--- docker/prod/web/entrypoint.sh
+++ docker/prod/web/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-# shellcheck disable=SC2086
 
 echo "Waiting for postgres..."
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   sleep 0.1
 done
 echo "PostgreSQL started"
