a36f587610b6235e9fefdf42efafede84000e23b
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-02-25 22:47:05 +0200

    fixed some issues

--- docker/prod/web/entrypoint.sh
+++ docker/prod/web/entrypoint.sh
@@ -11,6 +11,5 @@ python manage.py check --deploy
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py makemessages --all
 gunicorn src.wsgi:application --bind 0.0.0.0:8000 --reload
 exec "$@"
