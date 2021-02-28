3ed07bc537d34f4ed39882277635b0254afdfe01
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-01 00:23:13 +0200

    uvicorn worker

--- docker/prod/web/entrypoint.sh
+++ docker/prod/web/entrypoint.sh
@@ -11,5 +11,7 @@ python manage.py check --deploy
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-gunicorn src.wsgi:application --bind 0.0.0.0:8000 --reload
+
+gunicorn -w 1 -k uvicorn.workers.UvicornH11Worker src.asgi:application --reload --bind 0.0.0.0:8000 --log-level debug
+# gunicorn src.wsgi:application --bind 0.0.0.0:8000 --reload
 exec "$@"
