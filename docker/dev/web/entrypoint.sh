67707d5661bf75303752d08e6e6670deab837598
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-08 10:55:58 +0200

    added wait for db command to entrypoint

--- docker/dev/web/entrypoint.sh
+++ docker/dev/web/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
-while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
-  sleep 0.1
-done
-echo "PostgreSQL started"
+python manage.py wait_for_db
 
 python manage.py makemigrations
 python manage.py migrate
