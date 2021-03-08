be120b62778db86527019f163e5573a124ffccc8
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-08 23:44:55 +0200

    translation fields updated

--- web/main/models.py
+++ web/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
 
@@ -7,7 +8,7 @@ from .managers import UserManager
 class User(AbstractUser):
 
     username = None
-    email = models.EmailField('Email address', unique=True)
+    email = models.EmailField(_('Email address'), unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -15,8 +16,8 @@ class User(AbstractUser):
     objects = UserManager()
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
 
     def __str__(self):
         return self.email
