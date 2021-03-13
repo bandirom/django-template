e2ad868fe02f0e64a450effaebf98d3a14f989f6
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-13 15:17:47 +0200

    Added test swagger api

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 from django.views.generic import RedirectView
+from .views import UserView
 
 urlpatterns = [
     path('', login_required(RedirectView.as_view(pattern_name='admin:index'))),
+    path('user/', UserView.as_view()),
 ]
--- web/main/views.py
+++ web/main/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import GenericAPIView, get_object_or_404
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserView(GenericAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset(), id=self.request.user.id)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def get(self, request):
+        """Current user view
+
+            Using this construction you can load related fields (select_related and prefetch_related) in queryset
+        """
+
+        serializer = self.get_serializer(self.get_object())
+        return Response(serializer.data)
