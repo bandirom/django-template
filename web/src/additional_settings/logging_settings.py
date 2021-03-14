1c2292a98b518cbe30d58fd7b8f9981668a5c423
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-14 23:29:48 +0200

    upd

--- web/src/additional_settings/logging_settings.py
+++ web/src/additional_settings/logging_settings.py
@@ -1,14 +1,29 @@
-L
-OGGING = {
+from os import environ
+
+
+LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["default"]},
     "formatters": {
         "simple": {
             "format": "%(levelname)s %(message)s"
         },
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
-        }
+        },
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
+        },
+        "json": {
+            "()": "main.logging.JsonFormatter",
+            "datefmt": "%Y-%m-%dT%H:%M:%SZ",
+            "format": (
+                "%(asctime)s %(levelname)s %(lineno)s %(message)s %(name)s %(pathname)s %(process)d %(threadName)s"
+            ),
+        },
     },
     "handlers": {
         'console': {
@@ -16,6 +31,36 @@ OGGING = {
         },
         "null": {
             "class": "logging.NullHandler",
+        },
+        "default": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose" if environ.get('DEBUG') else "json",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server" if environ.get('DEBUG') else "json",
+        },
+    },
+    "loggers": {
+        "django": {
+            "level": "INFO",
+            "propagate": True
+        },
+        "django.server": {
+            "handlers": ["django.server"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "saleor": {
+            "level": "DEBUG",
+            "propagate": True
+        },
+        "saleor.graphql.errors.handled": {
+            "handlers": ["default"],
+            "level": "ERROR",
+            "propagate": False,
         },
     },
 
