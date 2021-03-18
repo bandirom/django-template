62b5df2d91da3ca6454ac78909c2f8047130f610
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-18 11:37:44 +0200

    added health check middleware

--- web/main/middleware.py
+++ web/main/middleware.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
+from django.conf import settings
 
 
 class RemoteUserMiddleware(MiddlewareMixin):
@@ -8,3 +10,9 @@ class RemoteUserMiddleware(MiddlewareMixin):
             request.user_id = int(user_id)
         elif request.user.is_authenticated:
             request.user_id = request.user.id
+
+
+class HealthCheckMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        if request.META["PATH_INFO"] == settings.LB_HEALTH_CHECK_URL:
+            return HttpResponse("pong")
