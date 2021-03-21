7caf0fe07294d4c2cc4baa42e73337930058ce2a
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-21 15:58:37 +0200

    from debug to info

--- docker/prod/web/entrypoint.sh
+++ docker/prod/web/entrypoint.sh
@@ -7,6 +7,6 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
 
-gunicorn -w 2 -k uvicorn.workers.UvicornH11Worker src.asgi:application --reload --bind 0.0.0.0:8000 --log-level debug
+gunicorn -w 2 -k uvicorn.workers.UvicornH11Worker src.asgi:application --reload --bind 0.0.0.0:8000 --log-level info
 # gunicorn src.wsgi:application --bind 0.0.0.0:8000 --reload
 exec "$@"
