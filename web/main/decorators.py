80293443478b618ae2179ce97f0fdef686e80646
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-03-26 11:22:34 +0200

    added decorator for time measuring

--- web/main/decorators.py
+++ web/main/decorators.py
@@ -2,6 +2,21 @@ import logging
 
 from requests.exceptions import RequestException
 from kombu.exceptions import OperationalError
+from timeit import default_timer
+
+logger = logging.getLogger(__name__)
+
+
+def execution_time(func) -> object:
+    def delta_time(*args, **kwargs):
+        t1 = default_timer()
+        data = func(*args, **kwargs)
+        delta = default_timer() - t1
+        logger.info(f'Function: {func.__name__}, Run time: {delta}')
+        logger.info(f'Returned data: {data}, Type: {type(data)}')
+        logger.info('############ SEPARATING ############')
+        return data
+    return delta_time
 
 
 def except_shell(errors=(Exception,), default_value=''):
