968ae68e0ad95b81dd4af69a7cdcd6bc005c6a8d
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-04-03 15:16:00 +0300

    Moved to library django-microservice-request 0.1.1

--- web/main/middleware.py
+++ web/main/middleware.py
@@ -3,15 +3,6 @@ from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
 
 
-class RemoteUserMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        request.user_id = None
-        if user_id := request.headers.get('Remote-User'):
-            request.user_id = int(user_id)
-        elif request.user.is_authenticated:
-            request.user_id = request.user.id
-
-
 class HealthCheckMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.META["PATH_INFO"] == settings.HEALTH_CHECK_URL:
