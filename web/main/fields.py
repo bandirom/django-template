fe348c7621015e2b9249f0a69e20a4a56fe726bf
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-04-22 23:03:10 +0300

    added IndentedJSONField for model

---
+++ web/main/fields.py
@@ -0,0 +1,10 @@
+from django.db.models import JSONField
+
+from .forms import IndentedJSONFormField
+
+
+class IndentedJSONField(JSONField):
+    """JsonField with indented lines"""
+    def formfield(self, **kwargs):
+        return super().formfield(**{'form_class': IndentedJSONFormField, **kwargs, })
+
