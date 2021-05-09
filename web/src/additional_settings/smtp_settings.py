e2ac430b4828f14eec268547672c75fc17749911
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-09 18:46:35 +0300

    update mailhog credential

---
+++ web/src/additional_settings/smtp_settings.py
@@ -0,0 +1,10 @@
+from os import environ
+
+EMAIL_HOST = environ.get("EMAIL_HOST")
+EMAIL_PORT = int(environ.get("EMAIL_PORT", 465))
+EMAIL_HOST_USER = environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = environ.get("DEFAULT_FROM_EMAIL")
+EMAIL_TIMEOUT = 15
+EMAIL_USE_SSL = int(environ.get("EMAIL_USE_SSL", 0))
+EMAIL_USE_TLS = int(environ.get("EMAIL_USE_TLS", 0))
