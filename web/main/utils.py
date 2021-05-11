34bce09566de8f583428fefb63067ef3cb5a7823
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-11 15:44:34 +0300

    added utils function find_by_key

--- web/main/utils.py
+++ web/main/utils.py
@@ -6,3 +6,16 @@ def parse_str_with_space(var: str) -> str:
     """
     str_list = list(filter(None, var.split(' ')))
     return ' '.join(x for x in str_list)
+
+
+def find_by_key(data: dict, target):
+    """Find a key value in a nested dict"""
+    for k, v in data.items():
+        if k == target:
+            return v
+        elif isinstance(v, dict):
+            return find_by_key(v, target)
+        elif isinstance(v, list):
+            for i in v:
+                if isinstance(i, dict):
+                    return find_by_key(i, target)
