aad7e8af305042c1adde4793757fc02a3bffe038
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-23 18:12:17 +0300

    gunicorn for unix socket

--- web/gunicorn.conf.py
+++ web/gunicorn.conf.py
@@ -1,10 +1,8 @@
 from multiprocessing import cpu_count
 
 
-bind = "0.0.0.0:8000"
-# bind = "unix:/gunicorn/gunicorn.sock"
 # workers = cpu_count() * 2 + 1
-
+bind = ["0.0.0.0:8000", "unix:/gunicorn_socket/gunicorn.sock"]
 workers = 3
 worker_class = 'uvicorn.workers.UvicornH11Worker'
 loglevel = 'info'
