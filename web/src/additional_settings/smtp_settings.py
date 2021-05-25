2d3726346a8fb0c3a13df4e7b574a356078ff936
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-05-25 09:47:02 +0300

    email timeout env

--- web/src/additional_settings/smtp_settings.py
+++ web/src/additional_settings/smtp_settings.py
@@ -5,6 +5,6 @@ EMAIL_PORT = int(environ.get("EMAIL_PORT", 465))
 EMAIL_HOST_USER = environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = environ.get("DEFAULT_FROM_EMAIL")
-EMAIL_TIMEOUT = 15
+EMAIL_TIMEOUT = int(environ.get("EMAIL_TIMEOUT", 15))
 EMAIL_USE_SSL = int(environ.get("EMAIL_USE_SSL", 0))
 EMAIL_USE_TLS = int(environ.get("EMAIL_USE_TLS", 0))
