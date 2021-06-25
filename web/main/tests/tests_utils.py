cb91481cda6e7143f05eb8f00c1c72f37cb6f614
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-06-25 09:51:49 +0300

    added find_dict_in_list utils func

--- web/main/tests/tests_utils.py
+++ web/main/tests/tests_utils.py
@@ -32,3 +32,33 @@ class UtilsTestCase(TestCase):
         self.assertEqual(utils.parse_str_with_space(str2), 'You are looking great')
         str3 = " This    double  life you    lead is   eating you   up from  within    "
         self.assertEqual(utils.parse_str_with_space(str3), 'This double life you lead is eating you up from within')
+
+    def test_find_dict_in_list(self):
+        list_1 = [
+            {
+                'key1': 'Test1',
+                'key2': 'test2',
+            },
+            {
+                'key1': 'Value2',
+                'key2': 'Valey3',
+            },
+            {
+                'key1': 1,
+                'key2': False,
+            },
+            {
+                'key1': 100500,
+                'key2': ['test1'],
+            },
+        ]
+        result = utils.find_dict_in_list(target=list_1, dict_key='key1', lookup_value='Test1')
+        self.assertEqual(result, list_1[0])
+        result = utils.find_dict_in_list(target=list_1, dict_key='key2', lookup_value='test2')
+        self.assertEqual(result, list_1[0])
+        result = utils.find_dict_in_list(target=list_1, dict_key='key1', lookup_value=100500)
+        self.assertEqual(result, list_1[3])
+        result = utils.find_dict_in_list(target=list_1, dict_key='key2', lookup_value=['test1'])
+        self.assertEqual(result, list_1[3])
+        result = utils.find_dict_in_list(target=list_1, dict_key='key2', lookup_value=False)
+        self.assertEqual(result, list_1[2])
--- web/main/utils.py
+++ web/main/utils.py
@@ -1,3 +1,5 @@
+from typing import Hashable
+
 
 def parse_str_with_space(var: str) -> str:
     """ return string without multiply whitespaces
@@ -19,3 +21,9 @@ def find_by_key(data: dict, target):
             for i in v:
                 if isinstance(i, dict):
                     return find_by_key(i, target)
+
+
+def find_dict_in_list(target: list[dict], dict_key: Hashable, lookup_value) -> dict:
+    """Find a dict in a list of dict by dict key"""
+    data = list(filter(lambda x: x[dict_key] == lookup_value, target))
+    return data[0] if data else {}
