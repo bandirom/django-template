c8a4eaf6c445e01f2e1b01c0e6af8b56de61ea79
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-06-27 02:16:14 +0300

    GUNICORN SETTINGS

--- web/gunicorn.conf.py
+++ web/gunicorn.conf.py
@@ -1,9 +1,24 @@
 from multiprocessing import cpu_count
+from os import environ
 
 
-# workers = cpu_count() * 2 + 1
 bind = ["0.0.0.0:8000", "unix:/gunicorn_socket/gunicorn.sock"]
-workers = 3
+
+workers = 3  # cpu_count() * 2 + 1
+
+threads = 1
+
 worker_class = 'uvicorn.workers.UvicornH11Worker'
+
 loglevel = 'info'
-reload = True
+accesslog = None  # environ.get('APP_HOME', '') + '/logs/gunicorn_access.log'
+
+errorlog = '-'  # environ.get('APP_HOME', '') + '/logs/gunicorn_errors.log'
+
+reload = False
+
+max_requests = 1000
+max_requests_jitter = 200
+
+user = 1000
+group = 1000
