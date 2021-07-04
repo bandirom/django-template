f960feeecd665c38013948e22e10af4cbc7f4a98
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-07-04 22:55:08 +0300

    improve tag

--- web/main/templatetags/main.py
+++ web/main/templatetags/main.py
@@ -3,12 +3,10 @@ from django.conf import settings
 
 register = template.Library()
 
-title = settings.MICROSERVICE_TITLE
-
 
 @register.simple_tag
 def microservice_title():
-    return title
+    return settings.MICROSERVICE_TITLE
 
 
 @register.simple_tag
