776b06b47ac7ec118b43450c642e0a2017524769
Nazarii Romanchenko <nazarii.romanchenko@stemsc.com>
2021-07-08 16:15:46 +0300

    added env configuration to gunicorn

--- web/gunicorn.conf.py
+++ web/gunicorn.conf.py
@@ -4,18 +4,18 @@ from os import environ
 
 bind = ["0.0.0.0:8000", "unix:/gunicorn_socket/gunicorn.sock"]
 
-workers = 3  # cpu_count() * 2 + 1
+workers: int = environ.get('GUNICORN_WORKERS', cpu_count() * 2 + 1)
 
-threads = 1
+threads: int = environ.get('GUNICORN_THREADS', 1)
 
 worker_class = 'uvicorn.workers.UvicornWorker'
 
 loglevel = 'info'
-accesslog = None  # environ.get('APP_HOME', '') + '/logs/gunicorn_access.log'
 
+accesslog = None  # environ.get('APP_HOME', '') + '/logs/gunicorn_access.log'
 errorlog = '-'  # environ.get('APP_HOME', '') + '/logs/gunicorn_errors.log'
 
-reload = False
+reload = bool(environ.get('GUNICORN_RELOAD', 0))
 
 # Reload gunicorn worker if request count > max_requests
 max_requests = 1000
@@ -23,3 +23,7 @@ max_requests_jitter = 200
 
 user = 1000
 group = 1000
+
+timeout: int = environ.get('GUNICORN_TIMEOUT', 30)
+
+keepalive: int = environ.get('GUNICORN_KEEP_ALIVE', 2)
