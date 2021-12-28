ac569206a36549c764dd7cfbb98fa3f2f54da637
Nazarii <romanchenko@procivis.ch>
2021-12-28 11:01:18 +0200

    fix test after merge

--- web/main/tests/tests_utils.py
+++ web/main/tests/tests_utils.py
@@ -4,26 +4,6 @@ from main import utils
 
 
 class UtilsTestCase(TestCase):
-    def test_find_by_key(self):
-        data = {
-            'tes': 't',
-            'dev': 'elop',
-            'dict': {
-                'AWS': {'founder': 'Jeffrey Preston', 'date': 2006},
-                'Tesla': {'founder': 'Elon Musk', 'date': 2003},
-            },
-        }
-        self.assertEqual(utils.find_by_key(data, 'tes'), 't')
-        self.assertEqual(utils.find_by_key(data, 'AWS'), data['dict']['AWS'])
-        self.assertEqual(utils.find_by_key(data, 'founder'), data['dict']['AWS']['founder'])
-        data = {
-            'list_dict': [
-                {'clouds': ['AWS', 'GCP', 'Azure', 'Digital Ocean']},
-                {'brands': ['Samsung', 'Tesla', 'Renault']},
-            ],
-        }
-        self.assertEqual(utils.find_by_key(data, 'clouds'), data['list_dict'][0]['clouds'])
-
     def test_parse_str_with_space(self):
         str1 = 'We are the champions'
         self.assertEqual(utils.parse_str_with_space(str1), str1)
