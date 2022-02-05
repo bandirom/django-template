9e435a4893afcff2afe8941ac46bb80cae55b4ad
bandirom <bandirom@ukr.net>
2022-02-05 18:11:09 +0200

    update requirements

--- web/main/pagination.py
+++ web/main/pagination.py
@@ -2,12 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class BasePageNumberPagination(PageNumberPagination):
-    page_size = 10
-    page_query_param = 'page'
-    max_page_size = 100
-    page_size_query_param = 'page_size'
-
-    def get_paginated_response(self, data):
-        response = super().get_paginated_response(data)
-        response.data.update(self.get_html_context())
-        return response
+    page_size: int = 10
+    page_query_param: str = 'page'
+    max_page_size: int = 100
+    page_size_query_param: str = 'page_size'
