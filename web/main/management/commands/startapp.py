f00b249f9eb77cc51e4a0a8ecccfa1dff836c36b
bandirom <bandirom@ukr.net>
2022-06-27 23:23:22 +0300

    mypy

--- web/main/management/commands/startapp.py
+++ web/main/management/commands/startapp.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 
 from django.core.management.commands import startapp
 
@@ -6,7 +7,7 @@ MANAGEMENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Command(startapp.Command):
-    def handle_template(self, template: any, subdir: str):
+    def handle_template(self, template: Any, subdir: str):
         if template is None:
             self.stdout.write('Using custom template.')
             template = os.path.join(MANAGEMENT_DIR, 'templates', 'app_template.zip')
