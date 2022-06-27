cb48a82860a11adeabd777d43714bbd14b9ad50a
bandirom <bandirom@ukr.net>
2022-06-27 23:25:20 +0300

    mypy

--- web/src/urls.py
+++ web/src/urls.py
@@ -18,11 +18,13 @@ urlpatterns = [
 urlpatterns += swagger_url
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += [
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    ]
     if settings.ENABLE_SILK:
-        urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+        urlpatterns.append(path('silk/', include('silk.urls', namespace='silk')))
     if settings.ENABLE_DEBUG_TOOLBAR:
         from debug_toolbar import urls
 
-        urlpatterns += [path('__debug__/', include(urls))]
+        urlpatterns.append(path('__debug__/', include(urls)))
