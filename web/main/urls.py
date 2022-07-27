7acfcd100b27c28a8cdfe707dffd7e6ebd521072
bandirom <bandirom@ukr.net>
2022-07-27 10:03:16 +0300

    weasyprint

--- web/main/urls.py
+++ web/main/urls.py
@@ -2,9 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path
 from django.views.generic import RedirectView
 
-from .views import SetUserTimeZone
+from .views import SetUserTimeZone, PDFDownloadView
 
 urlpatterns = [
     path('', login_required(RedirectView.as_view(pattern_name='admin:index'))),
     path('timezone/', SetUserTimeZone.as_view(), name='set_user_timezone'),
+    path('pdf/', PDFDownloadView.as_view(), name='set_user_timezone'),
 ]
--- web/main/views.py
+++ web/main/views.py
@@ -1,12 +1,15 @@
 from typing import TYPE_CHECKING
 
 from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from weasyprint import HTML, CSS
 
 from .serializers import SetTimeZoneSerializer
 
@@ -41,4 +44,18 @@ class SetUserTimeZone(GenericAPIView):
             value=serializer.data.get('timezone'),
             max_age=getattr(settings, 'TIMEZONE_COOKIE_AGE', 86400),
         )
+        return response
+
+
+class PDFDownloadView(GenericAPIView):
+
+    def get(self, request):
+        response = HttpResponse(content_type='application/pdf')
+        display = 'attachment'
+        filename = 'test.pdf'
+        response['Content-Disposition'] = f'{display};filename="{filename}"'
+        html = render_to_string('pdf.html')
+        HTML(string=html).write_pdf(response, stylesheets=[
+            CSS(settings.STATIC_ROOT + '/pdf.css')
+        ])
         return response
