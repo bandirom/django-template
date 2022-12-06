3c55fcedb61f994b72dfe93d9033b1a5de84af4b
Vladyslav Bilous <vladyslav.bilous@ibm.com>
2022-12-06 11:49:39 +0200

    Restored project structure

--- docker/dev/web/entrypoints/test.sh
+++ docker/dev/web/entrypoints/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 coverage erase
-coverage run manage.py test ../tests/ || exit
+coverage run manage.py test || exit
 coverage report --fail-under="$COVERAGE_THRESHOLD" || exit
 coverage xml
