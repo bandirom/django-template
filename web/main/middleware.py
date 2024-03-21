a7ba671a72284c300b78ac68117c3b7bdac66ce9
Nazarii <romanchenko@procivis.ch>
2024-03-21 18:47:04 +0200

    Remove pytz dependency

--- web/main/middleware.py
+++ web/main/middleware.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING, Optional
+from zoneinfo import ZoneInfo
 
-import pytz
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils import timezone
@@ -22,7 +22,7 @@ class TimezoneMiddleware:
 
     def __call__(self, request: 'HttpRequest'):
         if tzname := request.COOKIES.get(getattr(settings, 'TIMEZONE_COOKIE_NAME', 'timezone')):
-            timezone.activate(pytz.timezone(tzname))
+            timezone.activate(ZoneInfo(tzname))
         else:
             timezone.deactivate()
         return self.get_response(request)
