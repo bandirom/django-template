77fd9c0874bd644b5ef6b067c61547535978538c
Nazarii <romanchenko@procivis.ch>
2025-08-24 12:30:21 +0300

    Prod dockerfile

--- web/main/admin.py
+++ web/main/admin.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
